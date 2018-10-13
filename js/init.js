01458618f362df3a5bb1839d120e751d372f4a45
Dominik Olwig <supermagic999@web.de>
2018-10-13 18:42:11 +0100

    smooth-scroll, expandable project-card

--- js/init.js
+++ js/init.js
@@ -6,7 +6,21 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function scroll() {
-  // test: find y-position during scroll
-  console.log($(window).scrollTop());
+// <body onscroll="scroll()">
+// function scroll() {
+//   // test: find y-position during scroll
+//   console.log($(window).scrollTop());
+// }
+
+function slaveToggleClick() {
+  $('#slaveShortInfo').toggle();
+  $('#slaveMoreInfo').toggle();
 }
+
+$(".navbar a, .smoothScroll").click(function(e){
+  var $anchor = $(this);
+  $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+  }, 500);
+  e.preventDefault(); //this is the important line.
+});
