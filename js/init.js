2b6f8e456de2a44dd4863f2bbc13c1883b8b1fe8
Dominik Olwig <supermagic999@web.de>
2018-10-14 19:15:27 +0100

    scroll spy

--- js/init.js
+++ js/init.js
@@ -6,21 +6,28 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-// <body onscroll="scroll()">
-// function scroll() {
-//   // test: find y-position during scroll
-//   console.log($(window).scrollTop());
-// }
-
 function slaveToggleClick() {
   $('#slaveShortInfo').toggle();
   $('#slaveMoreInfo').toggle();
 }
 
-$(".navbar a, .smoothScroll").click(function(e){
+$(".arrow-smoothScroll").click(function(e){
   var $anchor = $(this);
   $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
+      scrollTop: $($anchor.attr('href')).offset().top - 120
   }, 500);
   e.preventDefault(); //this is the important line.
 });
+
+// scroll spy
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+  $('.scrollspy').on('scrollSpy:enter', function() {
+    $('.navbar').find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+  });
+  $('scrollspy').on('scrollSpy:exit', function(){
+    $('.navbar').find('a[href="#'+$(this).attr('id')+'"]').parent().removeClass('active');
+  });
+});
+
+
